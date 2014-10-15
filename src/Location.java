99125c89794efca03e5985e07127b616c83cd7ca
Harry Johnson <hj1012@doc.ic.ac.uk>
2014-10-15 12:30:38 +0100

    Location class to hold coordinates

--- src/Location.java
+++ src/Location.java
@@ -2,4 +2,19 @@
  * Created by hj1012 on 15/10/14.
  */
 public class Location {
+	private int x;
+	private int y;
+
+	public Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
 }

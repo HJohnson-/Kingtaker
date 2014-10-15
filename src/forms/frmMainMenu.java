1744bfaf65493d191d5cc4d5d795c5a0235a95a9
Jake Cracknell <jc4512@corona05.doc.ic.ac.uk>
2014-10-15 15:10:47 +0100

    added main menu buttons

--- src/forms/frmMainMenu.java
+++ src/forms/frmMainMenu.java
@@ -7,6 +7,9 @@ import javax.swing.*;
  */
 public class frmMainMenu {
     private JPanel panel1;
+    private JButton btnSinglePlayer;
+    private JButton btnLocalMP;
+    private JButton btnOnlineMP;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("frmMainMenu");

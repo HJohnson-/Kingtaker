47b4ec34d0088fcd5e32079cb92328a8c15048b4
Robert Prust <robertprust@hotmail.com>
2014-10-16 12:09:05 +0100

    Added an image field into pieces.

--- src/pieces/ChessPiece.java
+++ src/pieces/ChessPiece.java
@@ -3,25 +3,26 @@ import main.Location;
 import main.PieceType;
 import main.Board;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 /**
  * Created by rp1012 on 15/10/14.
  */
 abstract public class ChessPiece {
 
     protected Board board;
+    public String img;
     public PieceType type;
     public Location cords;
 
-    public ChessPiece(Board board, PieceType type, Location cords) {
+    public ChessPiece(Board board, PieceType type, Location cords, String img) {
         this.type = type;
         this.board = board;
         this.cords = cords;
-    }
-
-    public ChessPiece(Board board, PieceType type, int x, int y) {
-        this.type = type;
-        this.board = board;
-        this.cords = new Location(x, y);
+        this.img = img;
     }
 
 	//Returns true if the piece could move from from location to to location on its turn. Doesn't check if move would put

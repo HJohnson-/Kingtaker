a726591be355d91db524b2b74f1b103bca25b1c7
Robert Prust <robertprust@hotmail.com>
2014-10-16 12:08:39 +0100

    Basic main method to create a basic chess board.

--- src/main/KingtakerMain.java
+++ src/main/KingtakerMain.java
@@ -1,24 +1,16 @@
 package main;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.geom.Rectangle2D;
+import BasicChess.BasicChess;
 
 /**
  * Created by hj1012 on 15/10/14.
  */
-class Surface extends JPanel {
-
-
-
-}
-
 public class KingtakerMain extends JFrame {
 
     public static void main(String[] args) {
-
+        BasicChess bc = new BasicChess();
+        bc.drawBoard();
     }
 
 }

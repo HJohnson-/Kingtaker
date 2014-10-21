2ac57935ee282bd23a26d06b706ab0fbfcb3a2bc
QuGeShenMeMingZiHaoNe <daniel@dyn1202-142.wlan.ic.ac.uk>
2014-10-21 15:31:28 +0100

    Client to Server 80% done

--- src/ClientConnection/ClientRequestSender.java
+++ src/ClientConnection/ClientRequestSender.java
@@ -1,28 +1,60 @@
 package ClientConnection;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
  * Created by daniel on 14/10/20.
  */
+
+
 public class ClientRequestSender implements Serializable {
 
-    int requestType = -1;
-    String requestMessage = "Message Missing !!";
+    public enum ClientCommandCode {
+        GET_GAME_LIST,
+        AUTHENTICATE_USER,
+        CREATE_GAME,
+        REMOVE_GAME,
+        REPORT_PLAYER
+    }
 
-    public void process(int requestType, String requestMessage) {
+    String requestType = "-1";
+    String requestMessage = "Message Missing !!";
+    ConnectionToServer connection;
+    ObjectOutputStream outputStream;
+    ObjectInputStream inputStream;
+
+    public void send(String requestType, String requestMessage, ConnectionToServer connection) throws Exception {
+
 
         this.requestType = requestType;
         this.requestMessage = requestMessage;
+        this.connection = connection;
+        this.outputStream = connection.getOutputStream();
+
+
+        // build up our sending Message
+        StringBuilder sb = new StringBuilder();
+        sb.append(requestType);
+        sb.append(requestMessage);
+        String sendingMessage = sb.toString();
+
+        //write to output stream
+        outputStream.writeObject(sendingMessage);
+        System.out.println("Sending Message" + sendingMessage);
+
+        // flush
+        outputStream.flush();
+
+        System.out.println("Message sent");
+
+        //Message receive from server response
+        String input = (String) inputStream.readObject();
+
+        System.out.println("Reaction to Server Reply :" + input);
 
     }
-
-    public class RequestType {
-        public int LOGIN = 1;
-        public int MATCHING = 2;
-        public int CHESS_MOVEMENT = 3;
-    }
-
 
 
 }
--- src/ClientConnection/ConnectionToServer.java
+++ src/ClientConnection/ConnectionToServer.java
@@ -3,23 +3,26 @@ package ClientConnection;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-
 /**
  * Created by daniel on 14/10/20.
  */
 
-public class ConnectionToServer  {
+public class ConnectionToServer {
     // ip not set yet
-    private String serverIP = "66.249.64.0";
+    private String serverIP = "146.169.46.133";
     private ObjectOutputStream output;
     private ObjectInputStream input;
     private Socket socket;
     private int attempt = 0;
-    private int serverPort = 8088;
-    private int clientPort = 1234;
+    private int serverPort = 4444;
+    private int clientPort = 3333;
+    private String messageFromServer = "NULL";
+    private static final char MESSAGE_DELIMINATOR = ',';
 
 
-    public ObjectInputStream getInputStream() { return input;  }
+    public ObjectInputStream getInputStream() {
+        return input;
+    }
 
     public ObjectOutputStream getOutputStream() {
         return output;
@@ -53,17 +56,18 @@ public class ConnectionToServer  {
                 // testing if socket is null
                 if (socket == null) {
                     System.out.println("Socket Null, Connection fail");
-                }else{
+                } else {
                     System.out.println("Server Connected!");
                 }
-                attempt +=1;
+                attempt += 1;
             }
-
-        }catch(IOException e){
+            if (attempt == 10) {
+                System.out.println("Connection fail.. disconnected!!");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return this;
     }
+
 }
-
-

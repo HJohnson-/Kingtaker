a894bdecb0acb96e0e1314d441fd2238ef9c0ebd
Jake Cracknell <jc4512@texel02.doc.ic.ac.uk>
2014-10-23 12:17:27 +0100

    did OMS.sendMEssage method

--- src/ClientConnection/OpponentMessageSender.java
+++ src/ClientConnection/OpponentMessageSender.java
@@ -1,25 +1,44 @@
 package ClientConnection;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 /**
  * Created by jc4512 on 23/10/14.
  */
 public class OpponentMessageSender {
     private final int PORT = 4445;
-    private Socket socket;
+    private final int TIMEOUT_MS = 10000;
 
-    public OpponentMessageSender(InetAddress ip) {
-        try {
-            this.socket = new Socket(ip, PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private InetAddress inetAddress;
+
+
+    public OpponentMessageSender(InetAddress inetAddress) {
+        this.inetAddress = inetAddress;
     }
 
-    public void sendMessage() {
+    //Attempts to send message to other client.
+    //Returns true if successful, false if there were only timeouts or IO exceptions.
+    public boolean sendMessage(String msg) {
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < TIMEOUT_MS) {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(inetAddress, PORT), TIMEOUT_MS);
 
+                DataOutputStream clientWriter = new DataOutputStream(socket.getOutputStream());
+                clientWriter.writeBytes(msg);
+                clientWriter.close();
+                socket.close();
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 }

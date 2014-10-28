72c8c86da6b60bdfb611b3825a4836ffed115f76
Robert Prust <robertprust@hotmail.com>
2014-10-28 12:11:16 +0000

    Commented the abstract class ChessFrame.

--- src/graphics/ChessFrame.java
+++ src/graphics/ChessFrame.java
@@ -13,10 +13,20 @@ public abstract class ChessFrame extends JFrame {
     protected int width = 600, height = 600;
     protected ChessPanel panel = new BasicChessPanel(null);
 
+    /**
+     * Sets up the chess frame with default parameters.
+     */
     public ChessFrame() {
         initUI();
     }
 
+    /**
+     * Sets up all the parameters of the ChessFrame.
+     * @param title What to display in the title bar.
+     * @param width The width of the window.
+     * @param height The height of the window.
+     * @param panel The panel of the chess variant, which is drawn into the window.
+     */
     public ChessFrame(String title, int width, int height, ChessPanel panel) {
         this.title = title;
         this.width = width;
@@ -25,6 +35,9 @@ public abstract class ChessFrame extends JFrame {
         initUI();
     }
 
+    /**
+     * Part of initialising a JPanel, this function sets up the window.
+     */
     protected void initUI() {
         setTitle(title);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

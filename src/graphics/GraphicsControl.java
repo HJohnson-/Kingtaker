7079cf53215a7720c404c1c4bb4c7edb2283b854
Robert Prust <robertprust@hotmail.com>
2014-10-28 11:57:24 +0000

    Added comments to the GraphicsControl class.

--- src/graphics/GraphicsControl.java
+++ src/graphics/GraphicsControl.java
@@ -15,6 +15,12 @@ public class GraphicsControl implements Runnable {
     protected int totalSteps = 25;
     protected int animationTime = 50;
 
+    /**
+     * Converts the given locations from board co-ordinates to graphics co-ordinates.
+     * @param cur The current position of the piece.
+     * @param end The position the piece will be in at the end of the animation. NB this will equal current piece
+     *            when a piece is first created or when a piece is not moving.
+     */
     public GraphicsControl(Location cur, Location end) {
         this.curCords = new Location(cur.getX() * tools.CELL_WIDTH, cur.getY() * tools.CELL_HEIGHT);
         this.endCords = new Location(end.getX() * tools.CELL_WIDTH, end.getY() * tools.CELL_HEIGHT);
@@ -28,10 +34,18 @@ public class GraphicsControl implements Runnable {
         return curCords.getY();
     }
 
+    /**
+     * Sets the panel field, allowing the run function to call repaint to show the changes it makes.
+     * @param panel The panel to be redrawn after each animation step.
+     */
     public void givePanel(JPanel panel) {
         this.panel = panel;
     }
 
+    /**
+     * Converts the given co-ordinates from chess square (i.e. 0-7 for basic chess) to a graphics position.
+     * @param l The location which the piece will end up on, when the animation concludes.
+     */
     public void setGoal(Location l) {
         endCords = new Location(l.getX() * tools.CELL_WIDTH, l.getY() * tools.CELL_HEIGHT);
     }
@@ -40,6 +54,11 @@ public class GraphicsControl implements Runnable {
         return animating;
     }
 
+    /**
+     * A default animator for pieces, which makes them move in a straight line (possibly diagonal) from their start
+     * position to their end position. It will take totalSteps * animationTime milliseconds to complete the animation.
+     * This results in longer moves having faster pieces than short moves.
+     */
     @Override
     public void run() {
         int animationXStep = (endCords.getX() - curCords.getX()) / totalSteps;

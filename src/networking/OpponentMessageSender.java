12cd1a3377fce0873b4bb8079b0e8878a8d0fbf9
Jake Cracknell <jc4512@line30.doc.ic.ac.uk>
2014-11-07 12:38:47 +0000

    rewrote opponentmessagesender

--- src/networking/OpponentMessageSender.java
+++ src/networking/OpponentMessageSender.java
@@ -1,7 +1,9 @@
 package networking;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -9,9 +11,11 @@ import java.net.Socket;
 /**
  * Created by jc4512 on 23/10/14.
  */
+//TODO: make this extend MessageSender
 public class OpponentMessageSender {
     private final int PORT = 4445;
     private final int TIMEOUT_MS = 10000;
+    private final int RETRY_WAIT_MS = 500;
 
     private InetAddress inetAddress;
 
@@ -22,8 +26,9 @@ public class OpponentMessageSender {
 
     //Attempts to send message to other client.
     //Returns true if successful, false if there were only timeouts or IO exceptions.
-    public boolean sendMessage(String msg) {
+    public String sendMessage(String msg, boolean waitForResponse) {
         long startTime = System.currentTimeMillis();
+        String response = null;
         while (System.currentTimeMillis() - startTime < TIMEOUT_MS) {
             try {
                 Socket socket = new Socket();
@@ -33,13 +38,26 @@ public class OpponentMessageSender {
                 clientWriter.writeBytes(msg);
                 System.out.println("[localhost] sent to [" + socket.getInetAddress().getHostAddress() + "]: " + msg);
 
+                if (waitForResponse) {
+                    BufferedReader clientReader =
+                            new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    response = clientReader.readLine();
+                    System.out.println("[" + socket.getInetAddress().getHostAddress() + "] response: " + response);
+                    clientReader.close();
+                }
+
                 clientWriter.close();
                 socket.close();
-                return true;
+                return response;
             } catch (IOException e) {
                 System.out.println(e.getMessage());
+                try {
+                    Thread.sleep(RETRY_WAIT_MS);
+                } catch (InterruptedException e1) {
+                    return null;
+                }
             }
         }
-        return false;
+        return response;
     }
 }

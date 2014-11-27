86af45199df82f5f0b4c714f4bf31f48d9dc9ec4
Harry Johnson <hj1012@doc.ic.ac.uk>
2014-11-27 12:02:56 +0000

    Save/Load now works, and an undo button!

--- src/main/GameController.java
+++ src/main/GameController.java
@@ -6,10 +6,7 @@ import pieces.ChessPiece;
 import pieces.PieceDecoder;
 import variants.BasicChess.King;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,8 +22,9 @@ public class GameController {
 	private PieceDecoder decoder;
     public GameMode gameMode = GameMode.MULTIPLAYER_LOCAL;
     private boolean playerIsWhite = true;
-    private ChessAI ai = new MinimaxAI(!playerIsWhite, 3);
+    private ChessAI ai = new MinimaxAI(!playerIsWhite, 2);
     private ExecutorService executor = Executors.newSingleThreadExecutor();
+	private List<String> previousTurns;
 
 	public Board getBoard() {
 		return board;
@@ -45,6 +43,7 @@ public class GameController {
 		this.gameID = gameID;
 		this.decoder = decoder;
         this.gameMode = mode;
+		previousTurns = new ArrayList<String>();
     }
 
 	public GameController(Board board, PieceDecoder decoder, String code, GameMode mode) {
@@ -61,6 +60,7 @@ public class GameController {
 		String pieces = code.substring(startOfValue, endOfValue);
 		board.populateFromCode(pieces, decoder);
         this.gameMode = mode;
+		previousTurns = new ArrayList<String>();
 	}
 
     public ChessAI getAI() {
@@ -127,9 +127,12 @@ public class GameController {
             System.out.println("Invalid move: you cannot control this colour");
 			return false;
         }
-		//TODO CHANGE COMMENT TO REMOVE TODO STATEMENT
 
-        //Executes move, unless the piece... TODO: why is this a boolean? Does it always return true!
+
+		if(local && gameMode != GameMode.MULTIPLAYER_ONLINE) {
+			previousTurns.add(toCode());
+		}
+        //Executes move, unless the piece...
 		//If checkmate is detected, the game ends, otherwise the active player is switched.
         //If the move was executed locally, it is sent to the remote player via launcher.
         if (beingMoved.executeMove(targetLocation)) {
@@ -144,6 +147,7 @@ public class GameController {
             }
 
             if (!isWhitesTurn && gameMode == GameMode.SINGLE_PLAYER) {
+				System.out.println("AI MOVE TIME");
                 executor.submit(new DoAIMove(this));
             }
 
@@ -151,6 +155,18 @@ public class GameController {
 		}
 
         return false;
+	}
+
+	public void undo() {
+		int size = previousTurns.size();
+		if(size < 1) {
+			return;
+		}
+		String code = previousTurns.remove(size-1);
+		if (code == null) {
+			return;
+		}
+		load(code);
 	}
 
     /**
@@ -302,6 +318,7 @@ public class GameController {
 		int startOfValue = 4;
 		int endOfValue = code.indexOf('~', startOfValue);
 		currentTurn = Integer.decode(code.substring(startOfValue, endOfValue));
+		isWhitesTurn = currentTurn % 2 == 1;
 		startOfValue = endOfValue+3;
 		endOfValue = code.indexOf('$', startOfValue);
 		gameID = Integer.decode(code.substring(startOfValue, endOfValue));

7d6a5e4adc08e2efb558faf9dfbb122641fc9f80
JakeCracknell <jc4512@imperial.ac.uk>
2014-12-25 20:58:45 +0000

    removed unused client codes

--- src/networking/NetworkingCodes/ClientToClientCode.java
+++ src/networking/NetworkingCodes/ClientToClientCode.java
@@ -8,8 +8,6 @@ public class ClientToClientCode {
     public final static int SEND_MOVE = 8;
     public final static int JOIN_OPEN_GAME_REQUEST_OK = 7;
     public final static int JOIN_OPEN_GAME_REQUEST_NO = 6;
-    public final static int UNUSED4 = 4;
-    public final static int UNUSED5 = 5;
 
     public final static String DEL = ",";
 }
